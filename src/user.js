e1de20ff3b8e786cf6c5b18b1f2d8f7fcfe98e98
Steven Hedges <steven.hedges34@gmail.com>
2024-02-24 22:28:16 -0500

    Can update state

--- src/user.js
+++ src/user.js
@@ -1,5 +1,19 @@
+import { createComponent } from "../framework";
 import { div } from "../framework/element";
 import { onClick } from "../framework/event";
 
-export const User = ({ firstName, lastName }) =>
-    div`${onClick(() => alert(firstName))} Hello, ${firstName} ${lastName}!`;
+const initialState = { firstName: "John", lastName: "Doe" };
+
+// state mutating actions
+const methods = {
+    changeName: (state, firstName) => ({ ...state, firstName})
+};
+
+// Component template
+const template = ({ firstName, lastName, methods }) =>
+    div`${onClick(() =>
+        methods.changeName("Thomas")    
+    )} Hello, ${firstName} ${lastName}!`;
+
+// real world component
+export const User = createComponent({ template, methods, initialState });
