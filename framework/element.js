4da9d78ad10111608448476228632582796c2da5
Steven Hedges <steven.hedges34@gmail.com>
2024-02-24 21:06:12 -0500

    import snabbdom into element.js

--- framework/element.js
+++ framework/element.js
@@ -1,19 +1,17 @@
-// Tagged template literals
-// 1st arg are the Strs which correspond to the static strings: "Hello", " ", " !"
-// 2nd arg, spread param, converts every arg (any num) into an array
+import { h } from "snabbdom";
+
 const createElement = tagName => (strings, ...args) => ({ 
-    type: tagName, 
-    template: strings.reduce(
-        (acc, currentString, index) => acc + currentString + (args[index] || ""),
-        ""
-    ),
+    type: "element", 
+    template: h( 
+        tagName,
+        {},
+        strings.reduce(
+            (acc, currentString, index) => acc + currentString + (args[index] || ""),
+            ""
+        )
+    )
 });
 
 export const div = createElement("div");
 export const p = createElement("p");
 // TODO: add more elements
-
-// const template  = div`Hello, ${firstName} ${lastName}!`;
-// div is a tag which allows us to make computations on the following template literal
-// Available by default in ES6 
-// const { template } = p`Hello, ${firstName} ${lastName}!`
